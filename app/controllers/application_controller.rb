fb36224aced752f5fe32f1973813d68dd140f89a
Hansel López <halm29799@gmail.com>
2022-10-18 14:04:41 -0600

    feat: finish devise authentication and add home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource_or_scope)
+    # Redirect to home page after sign in
+    root_path
+  end
 end

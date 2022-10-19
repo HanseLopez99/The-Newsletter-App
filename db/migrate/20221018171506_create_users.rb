778b02fc59fa4b61e2bbc233f97b3c3ad8d7280e
Hansel López <halm29799@gmail.com>
2022-10-18 22:12:46 -0600

    fix: delete unused user devise columns

--- db/migrate/20221018171506_create_users.rb
+++ db/migrate/20221018171506_create_users.rb
@@ -3,9 +3,6 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :admin
-      t.string :last_seen_at
-      t.string :time_zone
 
       t.timestamps
     end

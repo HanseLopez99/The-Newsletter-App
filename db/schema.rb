f5c30944bd18d60d5548efaee5a7d298845266ab
Hansel López <halm29799@gmail.com>
2022-10-18 22:12:46 -0600

    fix: delete unused user devise columns

--- db/schema.rb
+++ db/schema.rb
@@ -17,9 +17,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_171607) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "admin"
-    t.string "last_seen_at"
-    t.string "time_zone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false

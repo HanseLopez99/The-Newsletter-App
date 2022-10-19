3814526e99cc5bb96a17be22dc119c5544b2b632
Hansel López <halm29799@gmail.com>
2022-10-19 09:57:17 -0600

    refactor: renaming project

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -41,7 +41,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'newsapp@gmail.com'
+  config.mailer_sender = 'newsletterapp@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'

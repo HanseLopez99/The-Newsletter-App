c9bc6f1833fe77147d5773c290d1277263c2489c
Hansel López <halm29799@gmail.com>
2022-10-19 12:21:19 -0600

    feat: redesign newsletter view

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,6 @@ class Post < ApplicationRecord
 
   # Validations
   validates :description, presence: true, length: { maximum: 1000 }
-  validates :likes, presence: true, numericality: { only_integer: true }
+  validates :likes, numericality: { only_integer: true }
   validates :newsletter_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_170554) do
 
   create_table "posts", force: :cascade do |t|
     t.string "description"
-    t.integer "likes"
+    t.integer "likes", default: 0
     t.bigint "newsletter_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

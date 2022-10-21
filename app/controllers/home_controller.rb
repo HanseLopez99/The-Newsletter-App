41f406079b498f5072defb93b68c6f916284198e
Hansel López <halm29799@gmail.com>
2022-10-21 11:10:45 -0600

    fix: show posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   # Get the posts which the user is subscribed to
   def index
-    @posts = Post.joins(:newsletter).where(newsletters: { user_id: current_user.id })
+    @posts = Post.all
   end
 
 end
